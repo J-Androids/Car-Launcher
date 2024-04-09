4213e8ef1025c77a1097b252d6e6130771d826a4
Julio Rodriguez <jrodriguez74909@gmail.com>
2024-04-09 06:57:41 +0000

    Revert "Merge Android 13 QPR1"
    Revert submission 2338131-tm-qpr1-merge
    
    Reason for revert: <AUTO READ HERE>
    
    Reverted changes: /q/submissionid:2338131-tm-qpr1-merge
    
    Change-Id: Iaac44a71e79deb63c113601b8a0847bd6562b8da

--- src/com/android/car/carlauncher/CarTaskView.java
+++ src/com/android/car/carlauncher/CarTaskView.java
@@ -16,17 +16,10 @@
 
 package com.android.car.carlauncher;
 
-import static android.app.ActivityTaskManager.INVALID_TASK_ID;
-
-import static com.android.car.carlauncher.TaskViewManager.DBG;
-
-import android.annotation.Nullable;
 import android.app.ActivityManager;
 import android.content.Context;
-import android.graphics.Rect;
-import android.util.Log;
-import android.util.SparseArray;
 import android.view.SurfaceControl;
+import android.view.View;
 import android.window.WindowContainerToken;
 import android.window.WindowContainerTransaction;
 
@@ -35,19 +28,11 @@ import com.android.wm.shell.TaskView;
 import com.android.wm.shell.common.SyncTransactionQueue;
 
 /**
- * CarLauncher version of {@link TaskView} which solves some CarLauncher specific issues:
- * <ul>
- * <li>b/228092608: Clears the hidden flag to make it TopFocusedRootTask.</li>
- * <li>b/225388469: Moves the embedded task to the top to make it resumed.</li>
- * </ul>
+ * CarLauncher version of {@link TaskView} which solves some CarLauncehr specific issues.
  */
 public class CarTaskView extends TaskView {
-    private static final String TAG = CarTaskView.class.getSimpleName();
-    @Nullable
     private WindowContainerToken mTaskToken;
     private final SyncTransactionQueue mSyncQueue;
-    private final SparseArray<Rect> mInsets = new SparseArray<>();
-    private boolean mTaskViewReadySent;
 
     public CarTaskView(Context context, ShellTaskOrganizer organizer,
             SyncTransactionQueue syncQueue) {
@@ -59,76 +44,22 @@ public class CarTaskView extends TaskView {
     public void onTaskAppeared(ActivityManager.RunningTaskInfo taskInfo, SurfaceControl leash) {
         mTaskToken = taskInfo.token;
         super.onTaskAppeared(taskInfo, leash);
-
-        applyInsets();
     }
 
+    /**
+     * Called when the visibility of the window containing the view has changed.
+     * @param visibility The new visibility of the window.
+     */
     @Override
-    protected void notifyInitialized() {
-        super.notifyInitialized();
-        if (mTaskViewReadySent) {
-            if (DBG) Log.i(TAG, "car task view ready already sent");
-            return;
+    protected void onWindowVisibilityChanged(int visibility) {
+        super.onWindowVisibilityChanged(visibility);
+        if (visibility == View.VISIBLE && mTaskToken != null) {
+            WindowContainerTransaction wct = new WindowContainerTransaction();
+            // Clears the hidden flag to make it TopFocusedRootTask: b/228092608
+            wct.setHidden(mTaskToken, /* hidden= */false);
+            // Moves the embedded task to the top to make it resumed: b/225388469
+            wct.reorder(mTaskToken, /* onTop= */ true);
+            mSyncQueue.queue(wct);
         }
-        onCarTaskViewInitialized();
-        mTaskViewReadySent = true;
-    }
-
-    /**
-     * Called only once when the {@link CarTaskView} is ready.
-     */
-    protected void onCarTaskViewInitialized() {}
-
-    /**
-     * Moves the embedded task over the embedding task to make it shown.
-     */
-    public void showEmbeddedTask() {
-        if (mTaskToken == null) {
-            return;
-        }
-        WindowContainerTransaction wct = new WindowContainerTransaction();
-        // Clears the hidden flag to make it TopFocusedRootTask: b/228092608
-        wct.setHidden(mTaskToken, /* hidden= */ false);
-        // Moves the embedded task to the top to make it resumed: b/225388469
-        wct.reorder(mTaskToken, /* onTop= */ true);
-        mSyncQueue.queue(wct);
-    }
-
-    // TODO(b/238473897): Consider taking insets one by one instead of taking all insets.
-    /**
-     * Set & apply the given {@code insets} on the Task.
-     */
-    public void setInsets(SparseArray<Rect> insets) {
-        mInsets.clear();
-        for (int i = insets.size() - 1; i >= 0; i--) {
-            mInsets.append(insets.keyAt(i), insets.valueAt(i));
-        }
-        applyInsets();
-    }
-
-    private void applyInsets() {
-        if (mInsets == null || mInsets.size() == 0) {
-            Log.w(TAG, "Cannot apply null or empty insets");
-            return;
-        }
-        if (mTaskToken == null) {
-            Log.w(TAG, "Cannot apply insets as the task token is not present.");
-            return;
-        }
-        WindowContainerTransaction wct = new WindowContainerTransaction();
-        for (int i = 0; i < mInsets.size(); i++) {
-            wct.addRectInsetsProvider(mTaskToken, mInsets.valueAt(i), new int[]{mInsets.keyAt(i)});
-        }
-        mSyncQueue.queue(wct);
-    }
-
-    /**
-     * @return the taskId of the currently running task.
-     */
-    public int getTaskId() {
-        if (mTaskInfo == null) {
-            return INVALID_TASK_ID;
-        }
-        return mTaskInfo.taskId;
     }
 }
